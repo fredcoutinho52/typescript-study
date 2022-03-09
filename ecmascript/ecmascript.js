da5c3e310754d4efcefa95709570acc555fb0aa7
Fred Coutinho <fredcoutinho52@gmail.com>
2022-03-09 09:58:49 -0300

    init ecmascript module

---
+++ ecmascript/ecmascript.js
@@ -0,0 +1,23 @@
+"use strict";
+// let e const
+let seraQuePode = "?";
+console.log(seraQuePode);
+let estaFrio = true;
+if (estaFrio) {
+    let acao = "Colocar o casaco";
+    console.log(acao);
+}
+const cpf = "123.456.789-00";
+// cpf = "234234";
+console.log(cpf);
+function revelar() {
+    var segredo = "interno";
+    console.log(segredo);
+}
+revelar();
+// console.log(segredo);
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log(i);
+//# sourceMappingURL=ecmascript.js.map
