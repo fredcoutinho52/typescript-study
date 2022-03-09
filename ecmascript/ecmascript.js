adb97f94a72630aae7252ea357c7d2850d53b97d
Fred Coutinho <fredcoutinho52@gmail.com>
2022-03-09 10:21:02 -0300

    arrow this

--- ecmascript/ecmascript.js
+++ ecmascript/ecmascript.js
@@ -20,4 +20,33 @@ for (var i = 0; i < 10; i++) {
     console.log(i);
 }
 console.log(i);
+// arrow function
+const somar = (n1, n2) => n1 + n2;
+console.log(somar(2, 2));
+const saudacao = () => console.log("olá");
+saudacao();
+const falarCom = (pessoa) => console.log("Olá " + pessoa);
+falarCom("Fred");
+// this
+function normalComThis() {
+    console.log(this);
+}
+const normalComThisEspecial = normalComThis.bind(2);
+normalComThisEspecial();
+console.log(this);
+const arrowComThis = () => console.log(this);
+arrowComThis();
+const arrowComThisEspecial = arrowComThis.bind(2);
+arrowComThisEspecial();
+// parâmetro padrão
+function contagemRegressiva(inicio = 5, fim = inicio - 5) {
+    console.log(inicio);
+    while (inicio > fim) {
+        inicio--;
+        console.log(inicio);
+    }
+    console.log("Fim!");
+}
+contagemRegressiva();
+contagemRegressiva(5);
 //# sourceMappingURL=ecmascript.js.map
--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -23,3 +23,40 @@ for (var i = 0; i < 10; i++) {
     console.log(i);
 }
 console.log(i);
+
+// arrow function
+const somar = (n1: number, n2: number): number => n1 + n2;
+console.log(somar(2, 2));
+
+const saudacao = () => console.log("olá");
+saudacao();
+
+const falarCom = (pessoa: string) => console.log("Olá " + pessoa);
+falarCom("Fred");
+
+// this
+function normalComThis() {
+    console.log(this);
+}
+
+const normalComThisEspecial = normalComThis.bind(2);
+normalComThisEspecial();
+
+console.log(this);
+const arrowComThis = () => console.log(this);
+arrowComThis();
+
+const arrowComThisEspecial = arrowComThis.bind(2);
+arrowComThisEspecial();
+
+// parâmetro padrão
+function contagemRegressiva(inicio: number = 5, fim: number = inicio - 5): void {
+    console.log(inicio);
+    while(inicio > fim) {
+        inicio--;
+        console.log(inicio);
+    }
+    console.log("Fim!");
+}
+contagemRegressiva();
+contagemRegressiva(5);
